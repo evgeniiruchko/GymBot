03d7a70e9b09c9b4b393424136623a20dc118ed3
ru4ko <gk1987@mail.ru>
2022-08-15 11:44:50 +0300

    init

---
+++ src/main/java/ru/gymbot/GymBotApplication.java
@@ -0,0 +1,13 @@
+package ru.gymbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gymbot/GymBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gymbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
